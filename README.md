f1dcf72f3c89b5367f3f892f73c463389d9972d1
andyquinterom <afquinteromoreano@gmail.com>
2022-01-17 15:43:10 -0500

    Crea nuevo esquema de imagenes y exportacion

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 # Manual de marca, logos e íconos
 
+Las imagenes el respositorio se derivan de los archivos `.svg` en la carpeta
+imagenes. El nombre de estos archivos svg son el ID de la imagen. De este se
+derivan archivos PNG exportados en diferentes resoluciones con el siguiente
+formato de nombre:
+
+`id_width.png`.
+
+- id: El ID del archivo SVG original
+- width: el número de pixeles de ancho de la imagen.
+
 En este repositorio se encuentran los logos oficiales y manuales de marca de
 ProyAis. El uso de estos logos e íconos es exclusivo para MD&CO Consuling Group
 S.A.S (NIT 901.119.781-5) y ProyAis S.A.S. 
---
+++ export.R
@@ -0,0 +1,34 @@
+library(glue)
+library(stringr)
+library(assertthat)
+archivos <- list.files("imagenes", recursive = TRUE, pattern = ".svg$", include.dirs = F)
+archivos <- file.path("imagenes", archivos)
+assertthat::assert_that(
+  !any(str_detect(archivos, "\\s")),
+  msg = "Hay archivos o carpetas con espacio en el nombre"
+)
+
+build <- function(svg, format, width) {
+  cat("\n", "Exportando", svg, "a", format, width, "px", "\n")
+  file_name <- str_remove(svg, "\\.svg$")
+  file_name <- glue("{ file_name }_w{ width }px.{ format }")
+  export_command <- glue("inkscape --export-filename={ file_name } -w { width } { svg }")
+  system(export_command, wait = TRUE)
+}
+
+resoluciones <- c(1500, 1000, 500)
+formatos <- c("png")
+
+for (nombre_archivo in archivos) {
+
+  for (formato in formatos) {
+
+    for (resolucion in resoluciones) {
+
+      build(nombre_archivo, formato, resolucion)
+
+    }
+
+  }
+
+}
